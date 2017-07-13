412c3f8f4795d49217d836475040f7e3558b4bc2
Siarhei Tuzhyk <Siarhei_Tuzhyk@epam.com>
2017-07-13 14:24:10 +0300

    Revert "Project fot Jenkins testing"
    This reverts commit 813143daa8ddcb0801a618f82acaaa003bc732a4.

--- HelloWorld/src/main/java/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Siarhei_Tuzhyk on 7/11/2017.
- */
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
