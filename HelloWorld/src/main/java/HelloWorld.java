813143daa8ddcb0801a618f82acaaa003bc732a4
Siarhei Tuzhyk <Siarhei_Tuzhyk@epam.com>
2017-07-13 12:26:02 +0300

    Project fot Jenkins testing

---
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Siarhei_Tuzhyk on 7/11/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
